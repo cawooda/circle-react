9084b06a5a578998028067dc88b7fa42f7eaa3b1
Andrew Cawood <cawooda@gmail.com>
2024-07-12 20:13:25 +1000

    fixed for deploy

--- src/server/src/app.js
+++ src/server/src/app.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 app.use(express.static("/client/dist"));
 
-app.use(express.static(path.join(__dirname, "../../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 module.exports = app;
